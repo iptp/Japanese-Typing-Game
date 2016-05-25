1940e8f4b13d7682a76a2e69c4f6f9019d1035f3
Gabriel Tiburcio <tibuurcio@gmail.com>
2016-05-25 13:29:46 +0900

    Version 2 half done

--- JapaneseTyping.java
+++ JapaneseTyping.java
@@ -11,6 +11,8 @@ public class JapaneseTyping extends World {
     private int wordIndex;
     
     String typing;
+    
+    public static int score;
    
     public JapaneseTyping() {    
         super(WIDTH, HEIGHT, CELLSIZE);
@@ -19,6 +21,7 @@ public class JapaneseTyping extends World {
         wordIndex = 0;
         addWordToScreen(wordIndex);
         typing = "";
+        score = 0;
     }
     
     public void started() {
@@ -28,6 +31,7 @@ public class JapaneseTyping extends World {
     }
     
     public void act() {
+        showText("Score: " + score, WIDTH-3, 0);
         showText(typing, WIDTH/2, HEIGHT - 2);
         
         String key = Greenfoot.getKey();
@@ -36,11 +40,15 @@ public class JapaneseTyping extends World {
             if(key == "space") {
                 typing = "";
             }
+            else if(typing.length() > 0 && key == "backspace") {
+                typing = typing.substring(0, typing.length()-1);
+            }
             else if(key.length() == 1 && checkRange(key.charAt(0))) {
                 typing += key.toLowerCase();
             }
             
             if(checkCorrectAnswer(typing)) {
+                score += list_of_words.get(wordIndex).getScore();
                 removeObject(list_of_words.get(wordIndex));
                 wordIndex++;
                 checkWin();
--- Word.java
+++ Word.java
@@ -6,12 +6,17 @@ public class Word extends Actor
     private String front, back;
     private int drawInterval;
     private int delay;
+    private int score;
     
     public Word(String front, String back) {
         this.front = front;
         this.back = back;
         this.drawInterval = 0;
-        this.delay = 70;
+        
+        int gameScore = JapaneseTyping.score;
+        this.delay = gameScore + 70 - (gameScore*2);
+        
+        this.score = this.front.length();
 
         GreenfootImage bg = new GreenfootImage(front, 32, Color.BLACK, null);
         setImage(bg);
@@ -31,5 +36,9 @@ public class Word extends Actor
     
     public String getBack() {
         return this.back;
+    }
+    
+    public int getScore() {
+        return this.score;
     }
 }
